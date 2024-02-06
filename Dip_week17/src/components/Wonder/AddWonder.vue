cd56737c8cfa85d3ad58b7920662c8e99390255d
Plaifah <praypar5689@gmail.com>
2024-02-06 16:18:08 +0700

    first commit

---
+++ Dip_week17/src/components/Wonder/AddWonder.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="container mt-4">
+        <div class="row">
+
+            <div class="col-md-5 mb-1 mx-auto">
+                <div class="card shadow">
+                    <div class="card-header p-0">
+                        <img src="https://static.wanderon.in/wp-content/uploads/2023/11/top-min.jpg" class="card-img-top" alt="wonders">
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6 mx-auto">
+                <div class="card shadow">
+                    <div class="card-header bg-primary">
+                        <span class="text-white fs-5">
+                            ลงทะเบียนสิ่งมหัศจรรย์ของโลก
+                        </span>
+                    </div>
+                    <div class="card-body text-success">
+                        <form @submit.prevent="addWonder">
+                            <div class="mb-1">
+                                <label for="">ชื่อสิ่งมหัศจรรย์ของโลก</label>
+                                <input type="text" class="form-control" v-model="wonderState.name" />
+                            </div>
+                            <div class="mb-1">
+                                <label for="">ตำแหน่ง ที่อยู่ สิ่งมหัศจรรย์ของโลก</label>
+                                <input type="text" class="form-control" v-model="wonderState.label" />
+                            </div>
+                            <div class="mb-1">
+                                <label for="">ลิงค์ภาพสิ่งมหัศจรรย์ของโลก</label>
+                                <input type="text" class="form-control" v-model="wonderState.imageUrl" />
+                            </div>
+
+                            <div class="mb-1">
+
+                                <button type="submit" class="btn btn-primary text-white shadow">
+                                    ลงทะเบียนสิ่งมหัศจรรย์ของโลก
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script setup>
+import axios from 'axios'
+import { reactive } from 'vue';
+import { useRouter } from 'vue-router';
+
+const wonderState = reactive({
+    name: '',
+    location: '',
+    imageUrl:''
+})
+
+const router = useRouter()
+
+const addWonder = async () => {
+    await axios.post(`http://localhost:3000/wonders`,wonderState)
+        .then(() => {
+            console.log(wonderState)
+            router.push({name:'wonders'})
+        })
+    alert('ลงทะเบียนสิ่งมหัศจรรย์ของโลก เรียบร้อยแล้ว')
+}
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Sarabun&display=swap');
+
+.container {
+    font-family: 'Sarabun', sans-serif;
+
+}
+
+.form-label {
+    color: green;
+}
+</style>
